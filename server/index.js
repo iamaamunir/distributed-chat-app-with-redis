ba1fa9c0f42623e4e33d24ec05f29bc7c8e0a297
Munir Abdullahi <abdullahimunir3@gmail.com>
2025-07-10 14:01:17 +0100

    feat: users can join a room

--- server/index.js
+++ server/index.js
@@ -18,18 +18,35 @@ const PORT = process.env.PORT;
 const server = http.createServer(app);
 const io = new Server(server);
 
-// 
+//
 io.on("connection", (socket) => {
   console.log("user connected");
+
+  // joining a room
+  socket.on("join_room", (room) => {
+    console.log(`Joining room: ${room}`);
+    socket.join(room);
+    socket.to(room).emit("server_message", `a user just joined ${room}`);
+  });
+
+  // sending a chat message
+  socket.on("send_message", ({ room, user, message }) => {
+    console.log(`[${room}] ${user}, ${message}`);
+    socket.to(room).emit("receive_message", { user, message });
+  });
+  // disconnect user when they close the tab
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 app.use(express.static(join(__dirname, "../client")));
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to the chat app</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Welcome to the chat app</h1>");
+// });
 
-app.get("/chat", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "../client") });
 });
 
